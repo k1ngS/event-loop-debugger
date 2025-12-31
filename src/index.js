f74b12032da3fa8d7cd0b217295fc5b6f3a35c86
Marcos Beltrão <marcos.beltrao@outlook.pt>
2025-12-30 22:11:16 -0300

    feat: adicionar seleção de exemplos de programação assíncrona

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { select, Separator } from "@inquirer/prompts";
+import { run } from "./executor.js";
+
+const file = await select({
+  message: "Select one example below",
+  choices: [
+    {
+      name: "1. Promise Chain",
+      value: "promise-chain",
+      description:
+        "A technique in asynchronous programming (especially Javascript) for executing a sequence of operations one after another, where each step waits for the previous one to finish.",
+    },
+    {
+      name: "2. Async/Await",
+      value: "async-await",
+      description:
+        "A modern programming syntax (in languages like Javascript, Python, C#) that simplifies handling asynchronous operations (tasks that don't finish immediately, like network requests) by making them look like regular, sequential code, without blocking the main program.",
+    },
+    {
+      name: "3. SetTimeout",
+      value: "settimeout",
+      description:
+        "A function or mechanism used in programming to schedule the execution of a specific task or function after a specified period of time has passed.",
+    },
+    {
+      name: "4. SetImmediate",
+      value: "setimmediate",
+      description:
+        "In Node.js schedules a function (callback) to run as soon as the current event loop cycle finishes its I/P polling phase, execution it before any setTimeout or setInterval scheduled for the next cycle, making it ideal for deferring tasks asynchronously to avoid blocking the event loop and ensure non-blocking I/O.",
+    },
+    {
+      name: "5. Mixed Events",
+      value: "mixed-events",
+      description: "It' a mixture of all of the above events.",
+    },
+  ],
+});
+
+run(file);
